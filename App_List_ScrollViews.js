9430d9dff9af1ecee989144714084a5e61f43f9b
moohoozaa <paramate.php@gmai.com>
2020-08-02 09:58:05 +0700

    Initial commit

---
+++ App_List_ScrollViews.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native'
+
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+  ])
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {people.map(item => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
+})
---
+++ App_TextInput.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+
+export default function App() {
+  const [name, setName] = useState('paramate')
+  const [age, setAge] = useState('30')
+
+  return (
+    <View style={styles.container}>
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={value => setName(value)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 99'
+        onChangeText={value => setAge(value)}
+      />
+      <Text>
+        name: {name}, age: {age}
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+})
